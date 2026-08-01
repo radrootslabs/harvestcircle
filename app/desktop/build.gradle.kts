3cb60476cbc6e4c3db8973b6d43f964d7b0964f6
triesap <tyson@radroots.org>
2026-08-01 18:41:10 +0000

    package: align the macOS alpha distribution
    - declare 0.1.0-alpha as the Gradle project version
    - limit native distribution output to the macOS DMG
    - retain the positive jpackage compatibility bundle version
    - identify the approved alpha release in package metadata

--- app/desktop/build.gradle.kts
+++ app/desktop/build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.gradle.api.tasks.testing.Test
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+version = "0.1.0-alpha"
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.compose.multiplatform)
@@ -38,15 +40,11 @@ compose.desktop {
         )
 
         nativeDistributions {
-            targetFormats(
-                TargetFormat.Dmg,
-                TargetFormat.Msi,
-                TargetFormat.Deb,
-            )
+            targetFormats(TargetFormat.Dmg)
 
             packageName = "Radroots"
             packageVersion = "1.0.0"
-            description = "Radroots Studio"
+            description = "Radroots Studio 0.1.0-alpha"
             copyright = "Copyright © 2024 Radroots, Inc."
             vendor = "Radroots, Inc"
 
